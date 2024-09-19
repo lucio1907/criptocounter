1aef9c4ae6759b9157295ff2d8ab14819f0d7c5e
Valu1911 <valenlucero406@gmail.com>
2024-09-19 18:19:00 -0300

    botones arreglados

--- client/src/components/NavButtons.tsx
+++ client/src/components/NavButtons.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const NavButtons = (): React.ReactElement => {
   return (
-    <div className="w-full h-[200px] px-3">
-      <ul className="flex justify-center mt-10 gap-10">
+    <div className="w-full h-[200px] px-4">
+      <ul className="flex justify-center mt-10 gap-4">
         <li className="flex flex-col">
-          <a href="/" className="w-full bg-slate-500 rounded-3xl p-3">
+          <a href="/" className="w-full bg-slate-500 rounded-3xl py-1  px-4">
             <img
               src="/logo_inicio_1.webp"
               alt="logo-inicio"
@@ -14,7 +14,7 @@ const NavButtons = (): React.ReactElement => {
           </a>
         </li>
         <li className="flex flex-col">
-          <a href="/" className="w-full bg-slate-500 rounded-3xl p-3">
+          <a href="/" className="w-full bg-slate-500 rounded-3xl py-1 px-4">
             <img
               src="/logo_menu_casino_1.webp"
               alt="logo-inicio"
@@ -23,7 +23,7 @@ const NavButtons = (): React.ReactElement => {
           </a>
         </li>
         <li className="flex flex-col">
-          <a href="/" className="w-full bg-slate-500 rounded-3xl p-3">
+          <a href="/" className="w-full bg-slate-500 rounded-3xl py-1 px-4">
             <img
               src="/logo_airdrop_1.webp"
               alt="logo-inicio"
@@ -33,8 +33,8 @@ const NavButtons = (): React.ReactElement => {
         </li>
         <li className="flex flex-col">
           <a
-           href="/" className="w-full bg-slate-500 rounded-3xl p-3">
-            <img src="/taskprueba.webp"
+           href="/" className="w-full bg-slate-500 rounded-3xl py-1 px-4">
+            <img src="/tasknuevo.webp"
              alt="Recompensas"
              className="w-[80px] h-[80px] object-contain">
             
